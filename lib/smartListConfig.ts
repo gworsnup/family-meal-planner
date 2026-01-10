4b3760faae89a621f7fcc6e27928020375b60d13
gworsnup <64284433+gworsnup@users.noreply.github.com>
2026-01-10 17:54:10 +0000

    Refactor smart list generation transaction

---
+++ lib/smartListConfig.ts
@@ -0,0 +1,12 @@
+export const SMART_LIST_CATEGORIES = [
+  "Produce",
+  "Meat & Fish",
+  "Dairy & Eggs",
+  "Bakery & Bread",
+  "Pantry (Condiments / Sauces)",
+  "Spices",
+  "Frozen",
+  "Canned / Jarred",
+  "Drinks",
+  "Other",
+];
