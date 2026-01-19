5e98b6a06cb721329ee810cf3ba8891ce42a49e9
Gareth Worsnup <gworsnup@gmail.com>
2026-01-19 22:45:50 +0000

    Load marketing template CSS in marketing layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,14 +1,17 @@
-import HeaderOne from "@/src/marketing/layouts/headers/HeaderOne";
+import Head from "next/head";
 
-export default function MarketingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
-      <HeaderOne />
-      <main style={{ flex: 1 }}>{children}</main>
-    </div>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/css/app.min.css" />
+        <link rel="stylesheet" href="/assets/css/main.css" />
+        <link rel="stylesheet" href="/assets/css/remixicon.css" />
+        <link rel="stylesheet" href="/assets/css/slick.css" />
+        <link rel="stylesheet" href="/assets/css/slick-theme.css" />
+      </Head>
+      {children}
+    </>
   );
 }
